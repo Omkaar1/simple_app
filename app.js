9c464a9f060c6f3b7617326ed24e782dc1da3067
Omkaar1 <imomkarlondhe@gmail.com>
2026-04-13 10:42:06 +0530

    optimised

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from Omkar DevOps engineer CI/CD 🚀");
+  res.end("Hi, my name is Omkar and i am a DevOps engineer 🚀");
 });
 
 server.listen(3000);

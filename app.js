18a714c7481f741a0a510e6a3615035c4174bad9
Omkar Londhe <87084951+Omkaar1@users.noreply.github.com>
2026-04-14 21:15:46 +0530

    Fix greeting message punctuation in app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("Hi, my name is Omkar and i am a DevOps engineer with 3 years of experience");
+  res.end("Hi, my name is Omkar and i am a DevOps engineer with 3 years of experience!");
 });
 
 server.listen(3000);
